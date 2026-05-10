3d7c3cbe9fb0371f4a096e54e21bcef558a4632e
PaulMick <paulwmick@gmail.com>
2026-05-10 17:05:26 -0400

    corners maybe working

--- solver.c
+++ solver.c
@@ -11,11 +11,12 @@ int is_solved(cube_3_state_t state) {
 char ctc(cube_3_state_t state, uint8_t pos_num, uint8_t face) {
     uint8_t corner = get_corner(state, pos_num);
     uint8_t rot = corner >> 3;
-    char color = corner_color[corner & 0b111][(rot - face) % 3];
-    printf("state: ");
-    print_binary64(state.corners);
-    printf("\n");
-    printf("pos_num: %d, color: %c, corner: %d, rot: %d\n", pos_num, color, corner, rot);
+    char color = corner_color[corner & 0b111][(rot + face) % 3];
+    // printf("state: ");
+    // print_binary64(state.corners);
+    // printf("\n");
+    // printf("pos_num: %d, color: %c, corner: %d, rot: %d, face: %d\n", pos_num, color, corner, rot, face);
+    // printf("ind2: %d\n", (rot + face) % 3);
     return color;
 }
 
@@ -28,15 +29,15 @@ uint8_t get_corner(cube_3_state_t state, uint8_t pos_num) {
 }
 
 void print_cube_3_state(cube_3_state_t st) {
-    printf("   %c%c%c   \n", '-', '-', '-');
+    printf("   %c%c%c   \n", ctc(st, 6, 2), '-', ctc(st, 7, 1));
     printf("   %c%c%c   \n", '-', 'O', '-');
-    printf("   %c%c%c   \n", '-', '-', '-');
-    printf("%c%c%c%c%c%c%c%c%c\n", '-', '-', '-', ctc(st, 2, 0), '-', ctc(st, 3, 0), '-', '-', '-');
+    printf("   %c%c%c   \n", ctc(st, 2, 1), '-', ctc(st, 3, 2));
+    printf("%c%c%c%c%c%c%c%c%c\n", ctc(st, 6, 1), '-', ctc(st, 2, 2), ctc(st, 2, 0), '-', ctc(st, 3, 0), ctc(st, 3, 1), '-', ctc(st, 7, 2));
     printf("%c%c%c%c%c%c%c%c%c\n", '-', 'G', '-', '-', 'W', '-', '-', 'B', '-');
-    printf("%c%c%c%c%c%c%c%c%c\n", '-', '-', '-', ctc(st, 0, 0), '-', ctc(st, 1, 0), '-', '-', '-');
-    printf("   %c%c%c   \n", ctc(st, 0, 2), '-', '-');
+    printf("%c%c%c%c%c%c%c%c%c\n", ctc(st, 4, 2), '-', ctc(st, 0, 1), ctc(st, 0, 0), '-', ctc(st, 1, 0), ctc(st, 1, 2), '-', ctc(st, 5, 1));
+    printf("   %c%c%c   \n", ctc(st, 0, 2), '-', ctc(st, 1, 1));
     printf("   %c%c%c   \n", '-', 'R', '-');
-    printf("   %c%c%c   \n", '-', '-', '-');
+    printf("   %c%c%c   \n", ctc(st, 4, 1), '-', ctc(st, 5, 2));
     printf("   %c%c%c   \n", ctc(st, 4, 0), '-', ctc(st, 5, 0));
     printf("   %c%c%c   \n", '-', 'Y', '-');
     printf("   %c%c%c   \n", ctc(st, 6, 0), '-', ctc(st, 7, 0));
